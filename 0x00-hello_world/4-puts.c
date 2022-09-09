b1680b8077d08d09f8366437dec467ab760ecee7
otomdee <otomdaniel65@gmail.com>
2022-09-08 21:07:41 -0700

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - main function
+ * Return: return function
+ */
+
 int main(void)
+
 {
-	char str[] = "Programming is like building a multilingual puzzle";
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str);
-	return(0);
+	return (0);
 }
