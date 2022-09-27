4c38ca5ba45e34ede6f4ec077db93abf89b858bb
otomdee <otomdaniel65@gmail.com>
2022-09-26 20:21:47 -0700

    task 4, 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,17 +7,18 @@
  * Return: num of bytes of s in accept
  */
 
-unsigned int _strspn(char *s, char 8accept)
+unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
+
 	for (i = 0; s[i]; i++)
 	{
 		for (j = 0; accept[j]; j++)
 		{
-			if (s[i] == accept[j]; j++)
+			if (s[i] == accept[j])
 				break;
 		}
-		if (!accpet[j])
+		if (!accept[j])
 			break;
 	}
 	return (i);
