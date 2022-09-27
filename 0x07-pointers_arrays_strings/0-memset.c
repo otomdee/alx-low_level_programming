c4c2f07e3090dbc9522e4adb70e86a261f2b0b99
otomdee <otomdaniel65@gmail.com>
2022-09-26 18:11:46 -0700

    retry

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
- * *_memset - fills mem
+ **_memset - fills mem
  * @s: mem area
  * @b: char
  * @n: num of times to copy
+ *
+ * Return: return value
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i , n; i++)
+	for (i = 0; i, n; i++)
 	{
 		s[i] = b;
 	}
