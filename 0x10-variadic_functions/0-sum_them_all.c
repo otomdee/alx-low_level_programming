6e20190b7ace17043d80c8c92c32047517c18428
otomdee <otomdaniel65@gmail.com>
2022-10-14 03:26:49 -0700

    task 1 redo

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,21 +7,16 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	/* creating va_list to store the variable argument list */
-	va_list my_nums;
-	unsigned int count;
-	int sum;
+	unsigned int i;
+	int sum = 0;
+	va_list list;
 
-	if (n == 0)
-		return (0);
-	sum = 0;
+	va_start(list, n);
 
-	/* init valist for the number of arguments */
-	va_start(my_nums, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(list, int);
 
-	/* loop through/access all arguments stored in the valist */
-	for (count = 0; count < n; count++)
-		sum += va_arg(my_nums, int);
-	va_end(my_nums);
+	va_end(list);
+
 	return (sum);
 }
